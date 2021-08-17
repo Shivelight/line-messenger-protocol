0ff3be8949105fd4514ff7b564bc9b7fdbeb57a5
ii64 <26342994+ii64@users.noreply.github.com>
2021-08-18 03:52:59 +0700

    scripts: update build script for linego
    Signed-off-by: ii64 <26342994+ii64@users.noreply.github.com>

--- scripts/build_go_default.bat
+++ scripts/build_go_default.bat
@@ -1,2 +1,2 @@
 @echo off
-build_thrift -gen go:"thrift_import=github.com/ii64/linego/lib/thrift,package_prefix=github.com/ii64/linego/libgen/"
+build_thrift -bin "thrift-0.14.2.exe" -gen go:"thrift_import=github.com/ii64/linego/lib/thrift,package_prefix=github.com/ii64/linego/lib/gen/"
